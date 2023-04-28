c720bc1c23c08dc7c17ecb0efc39448cf28454cd
Kal Zheng <kal.zheng@inspire-dt.com>
2023-04-28 16:47:19 +0800

    add prop to pass back to parent thorugh slot so they can access it

--- src/App.vue
+++ src/App.vue
@@ -8,27 +8,37 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals>
+      <template #default="slotProps">
+        <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps.anotherProp }}</p>
+        <p>{{ slotProps.camelProp }}</p>
+        <!-- here can use camelCase 
+          for access the - (dash) -->
+      </template>
+    </course-goals>
   </div>
 </template>
 
 <script>
-import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
-
+import TheHeader from "./components/TheHeader.vue";
+import BadgeList from "./components/BadgeList.vue";
+import UserInfo from "./components/UserInfo.vue";
+import CourseGoals from "./components/CourseGoals.vue";
 
 export default {
   components: {
     TheHeader, //'the-header': TheHeader
     UserInfo,
     BadgeList,
+    CourseGoals,
   },
   data() {
     return {
       activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
       },
     };
   },
