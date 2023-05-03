6572203ee09c1633d51eaa5a0ce9a96110bfab1e
Kal Zheng <kal.zheng@inspire-dt.com>
2023-05-03 14:24:19 +0800

    user teleport to move element to where you want it

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -3,11 +3,16 @@
     <h2>Manage Gaols</h2>
     <input type="text" ref="goal" />
     <button @click="setGoal">Set Goal</button>
-    <error-alert v-if="inputIsInvalid">
-      <h2>Input is invalid</h2>
-      <p>Please Enter atleast a few characters...</p>
-      <button @click="confirmError">Okay</button>
-    </error-alert>
+    <!-- issue with this is that it is under the button in html in 
+        browser inspect might cause css error -->
+        <!-- hence we use teleport to move to outside -->
+    <teleport to="body" >
+        <error-alert v-if="inputIsInvalid">
+          <h2>Input is invalid</h2>
+          <p>Please Enter atleast a few characters...</p>
+          <button @click="confirmError">Okay</button>
+        </error-alert>
+    </teleport>
   </div>
 </template>
 
